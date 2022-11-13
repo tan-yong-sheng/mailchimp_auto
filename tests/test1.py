b3fd6768eb01915d2e515da385bdf76bd972772e
Tan Yong Sheng <tys203831@gmail.com>
2022-11-13 10:48:40 +0800

    Delete tests directory

--- tests/test1.py
+++
@@ -1,8 +0,0 @@
-from tempfile import template
-from mailchimp_auto.scripts.gspread_data import getGspreadData
-
-
-# spreadsheet_url = "https://docs.google.com/spreadsheets/d/1h31GQRfBqUpMmPCXI013J1-Y-kinfgifN6Pa3dx3hB0/edit?usp=sharing"
-
-
-    
